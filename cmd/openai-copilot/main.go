615bdc9fbfddfbbfe521e64737f78f8848995b42
rusthigh <rusthigh@proton.me>
2022-05-31 08:15:02 +0000

    Logs

--- cmd/openai-copilot/main.go
+++ cmd/openai-copilot/main.go
@@ -80,4 +80,4 @@ func chat() {
 // init initializes the command line flags
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&model, "model", "m", "gpt-4", "OpenAI model to use")
-	rootCmd.PersistentFlags().StringVarP(&prompt, "prom
+	rootCmd.PersistentFlags().StringVarP(&prompt, "prompt", "p", "", "Prompts sent to GPT model for no
