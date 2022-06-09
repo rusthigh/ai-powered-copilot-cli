98fa9c9870a8c02c5d169ba22788c0750272a295
rusthigh <rusthigh@proton.me>
2022-06-09 16:38:18 +0000

    Update MultiInstaller.sh

--- pkg/assistants/simple.go
+++ pkg/assistants/simple.go
@@ -33,3 +33,50 @@ func Assistant(model string, prompts []openai.ChatCompletionMessage, maxTokens i
 			count := llms.NumTokensFromMessages(chatHistory, model)
 			color.Green("Total tokens: %d\n\n", count)
 		}
+	}()
+
+	if verbose {
+		color.Blue("Iteration 1): chatting with LLM\n")
+	}
+
+	resp, err := client.Chat(model, maxTokens, chatHistory)
+	if err != nil {
+		return "", chatHistory, fmt.Errorf("chat completion error: %v", err)
+	}
+
+	chatHistory = append(chatHistory, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleAssistant,
+		Content: string(resp),
+	})
+
+	if verbose {
+		color.Cyan("Initial response from LLM:\n%s\n\n", resp)
+	}
+
+	var toolPrompt tools.ToolPrompt
+	if err = json.Unmarshal([]byte(resp), &toolPrompt); err != nil {
+		if verbose {
+			color.Cyan("Unable to parse tool from prompt, assuming got final answer: %s\n\n", resp)
+		}
+		return resp, chatHistory, nil
+	}
+
+	iterations := 0
+	if maxIterations <= 0 {
+		maxIterations = defaultMaxIterations
+	}
+	for {
+		iterations++
+
+		if verbose {
+			color.Cyan("Thought: %s\n\n", toolPrompt.Thought)
+		}
+
+		if iterations > maxIterations {
+			color.Red("Max iterations reached")
+			return toolPrompt.FinalAnswer, chatHistory, nil
+		}
+
+		if toolPrompt.FinalAnswer != "" {
+			if verbose {
+				color.Cyan("Final answer: %s\n\n", toolPrompt.FinalAnswer)
