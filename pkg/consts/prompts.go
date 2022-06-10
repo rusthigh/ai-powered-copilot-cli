85db20ba3f29807c5dc5a37c0df828ba9cbac7cf
rusthigh <rusthigh@proton.me>
2022-06-10 18:40:42 +0000

    Translated by qhwdw

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -3,4 +3,4 @@ package consts
 // DefaultPrompt is the default prompt for the AI assistant.
 const DefaultPrompt = `You are a helpful AI assistant. Answer the following questions
 as best you can. You have access to the following tools and please ensure they are
-lever
+leveraged when you are unsure of the respo
