6d32265a26a4da02d5acc2be7db1eb6c99c8b54b
rusthigh <rusthigh@proton.me>
2022-06-10 08:40:01 +0000

    gulp build

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -2,4 +2,5 @@ package consts
 
 // DefaultPrompt is the default prompt for the AI assistant.
 const DefaultPrompt = `You are a helpful AI assistant. Answer the following questions
-as best you can. You have access to the foll
+as best you can. You have access to the following tools and please ensure they are
+lever
