e3b5091bebb2d1d8d120ce057e1746afe9e4d921
rusthigh <rusthigh@proton.me>
2022-06-13 09:58:15 +0000

    Oops typo

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -5,4 +5,4 @@ const DefaultPrompt = `You are a helpful AI assistant. Answer the following ques
 as best you can. You have access to the following tools and please ensure they are
 leveraged when you are unsure of the responses:
 
-- search: a search engine. useful for when you need to answer questions about current events. i
+- search: a search engine. useful for when you need to answer questions about current events. input should be a search query. output i
