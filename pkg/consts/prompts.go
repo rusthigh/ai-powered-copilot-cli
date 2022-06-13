38e15023960e3289cc4c1d28f9d6af671d9b7f36
rusthigh <rusthigh@proton.me>
2022-06-13 18:23:04 +0000

    Add projects page

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -6,4 +6,4 @@ as best you can. You have access to the following tools and please ensure they a
 leveraged when you are unsure of the responses:
 
 - search: a search engine. useful for when you need to answer questions about current events. input should be a search query. output is the top search result.
-- python: a python interpreter. useful for executing Python code with Kubernetes Python SDK client. The results should be print out by calling "print(...)". input should be a pyt
+- python: a python interpreter. useful for executing Python code with Kubernetes Python SDK client. The results should be print out by calling "print(...)". input should be a python script. output is the stdout and stderr of the p
