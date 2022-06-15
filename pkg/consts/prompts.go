ec008f71c11bde009d0fbfb9a6d6f1b99e351781
rusthigh <rusthigh@proton.me>
2022-06-15 16:06:28 +0000

    Fix #40

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -17,4 +17,5 @@ Use this JSON format for responses:
 		"name": "<action to take, choose from tools [kubectl, python, trivy]. Do not set final_answer when an action is required>",
 		"input": "<input for the action. ensure all contexts are added as input if required, e.g. raw YAML or image name.>"
 	},
-	"observation": "<result of t
+	"observation": "<result of the action, set by external tools>",
+	"final
