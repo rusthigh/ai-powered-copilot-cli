846e8ef1e2896ff07ae4e762f97a675dd451b397
rusthigh <rusthigh@proton.me>
2022-06-16 14:08:37 +0000

    Clean up comment

--- pkg/llms/openai.go
+++ pkg/llms/openai.go
@@ -2,3 +2,49 @@
 package llms
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"math"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+type OpenAIClient struct {
+	*openai.Client
+
+	Retries int
+	Backoff time.Duration
+}
+
+// NewOpenAIClient returns an OpenAI client.
+func NewOpenAIClient() (*OpenAIClient, error) {
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	if apiKey == "" {
+		return nil, fmt.Errorf("OPENAI_API_KEY is not set")
+	}
+
+	config := openai.DefaultConfig(apiKey)
+	baseURL := os.Getenv("OPENAI_API_BASE")
+	if baseURL != "" {
+		config.BaseURL = baseURL
+
+		if strings.Contains(baseURL, "azure") {
+			config.APIType = openai.APITypeAzure
+			config.APIVersion = "2024-02-01"
+			config.AzureModelMapperFunc = func(model string) string {
+				return regexp.MustCompile(`[.:]`).ReplaceAllString(model, "")
+			}
+		}
+	}
+
+	return &OpenAIClient{
+		Retries: 5,
+		Backoff: time.Second,
+		Client:  openai.NewClientWithConfig(config),
+	}, nil
+}
