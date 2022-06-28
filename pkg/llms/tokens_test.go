7b193bd6638239eccf717e60e8ec6b3552d02655
rusthigh <rusthigh@proton.me>
2022-06-28 17:33:24 +0000

    New translations template.pot (Greek)

--- pkg/llms/tokens_test.go
+++ pkg/llms/tokens_test.go
@@ -37,3 +37,46 @@ func TestGetTokenLimits(t *testing.T) {
 		})
 	}
 }
+
+func TestConstrictPrompt(t *testing.T) {
+	type args struct {
+		prompt      string
+		model       string
+		tokenLimits int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "gpt-3.5-turbo-0613",
+			args: args{
+				prompt:      "This is a test prompt.",
+				model:       "gpt-3.5-turbo-0613",
+				tokenLimits: 512,
+			},
+			want: "This is a test prompt.",
+		},
+		{
+			name: "gpt-3.5-turbo-0613",
+			args: args{
+				prompt:      "This is a test prompt.",
+				model:       "gpt-3.5-turbo-0613",
+				tokenLimits: 1,
+			},
+			want: "",
+		},
+		{
+			name: "gpt-3.5-turbo-0613",
+			args: args{
+				prompt:      "This is a test prompt.\nhere is another.",
+				model:       "gpt-3.5-turbo-0613",
+				tokenLimits: 15,
+			},
+			want: "here is another.",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ConstrictPrompt(tt.args.prompt, tt.args.model, tt.args.tokenLimits); got != tt.want {
