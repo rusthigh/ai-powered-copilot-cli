0deb5664e5ce8da1b5af5ef8ef794f04f0d4ba7a
rusthigh <rusthigh@proton.me>
2022-07-08 07:48:18 +0000

    Updated readme [skip ci]

--- pkg/tools/googlesearch.go
+++ pkg/tools/googlesearch.go
@@ -9,4 +9,5 @@ import (
 	option "google.golang.org/api/option"
 )
 
-// GoogleSearch returns the results of a Google search for the give
+// GoogleSearch returns the results of a Google search for the given query.
+func GoogleSearch(query string) (s
